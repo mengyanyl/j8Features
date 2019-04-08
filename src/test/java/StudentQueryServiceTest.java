725d72d79af4c6cc36b49bda305ee4e17e0e2381
mengyan.li <mengyan.li@email.com>
2019-04-08 15:17:13 +0800

    增加了例子

--- src/test/java/StudentQueryServiceTest.java
+++ src/test/java/StudentQueryServiceTest.java
@@ -8,11 +8,14 @@ import org.myan.examples.service.cnNameFilterImpl;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.comparingDouble;
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by myan on 2019/3/12.
@@ -87,7 +90,7 @@ public class StudentQueryServiceTest {
     @Test
     public void testFilterByStreaming(){
         studentQueryService.getStudentList().stream()
-                .filter(student -> student.getAge()>12 && student.getAge()<14).collect(Collectors.toList());
+                .filter(student -> student.getAge()>12 && student.getAge()<14).collect(toList());
     }
 
     @Test
@@ -123,4 +126,56 @@ public class StudentQueryServiceTest {
         }, (cast -> System.out.println("exec time: " + cast/1000.00 + "s")));
     }
 
+    public void testSorByAvgScore(){
+        studentQueryService.getStudentList().sort(new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return Double.compare(o1.getAvgScore(), o2.getAvgScore());
+            }
+        });
+
+        studentQueryService.getStudentList().sort((o1, o2) -> {
+            return Double.compare(o1.getAvgScore(), o2.getAvgScore());
+        });
+
+        Comparator<Student> comparator = comparingDouble((Student s) -> s.getAvgScore());
+        studentQueryService.getStudentList().sort(comparingDouble(s -> s.getAvgScore()));
+
+        studentQueryService.getStudentList().sort(comparingDouble(Student::getAvgScore));
+    }
+
+    public void testLambdaInPractice(){
+        //得到平局分数大于60的学生，按照名字排序
+        List<Student> list = studentQueryService.getStudentList().stream().filter(s -> s.getAvgScore() > 60)
+                .sorted(comparing(Student::getCnName))
+                .collect(toList());
+
+        //返回所有学生名字符串，名字排序后，用“，”分割
+        String str = studentQueryService.getStudentList().stream().map(s -> s.getCnName())
+                .sorted()
+                .distinct()
+                .reduce("", (a, b) -> a + b + ", ");
+
+        //是否存在年龄小于18岁的学生
+        boolean isExist = studentQueryService.getStudentList().stream().anyMatch(s -> s.getAge() < 18);
+
+        //打印年龄小于18岁的学生姓名
+        studentQueryService.getStudentList().stream().filter(s -> s.getAge() < 18)
+                .map(Student::getCnName)
+                .forEach(System.out::println);
+
+        //得到最高的平均分
+        Optional<Double> highestAvgScore = studentQueryService.getStudentList().stream()
+                .map(Student::getAvgScore)
+                .reduce(Double::max);
+        //Optional<T>可以有个默认值，在没有最大值的情况下
+        highestAvgScore.orElse(1d);
+
+        //得到平均分最小的学生记录
+        studentQueryService.getStudentList().stream()
+                .reduce((a, b) -> a.getAvgScore() < b.getAvgScore() ? a : b);
+        //得到平均分最小的学生记录 更好的操作
+        studentQueryService.getStudentList().stream()
+                .min(comparing(Student::getAvgScore));
+    }
 }
